c4a5810ebd2d0bbcac304d34a9c86c08d4a1cf01
Heongle <lekueh@gmail.com>
2020-10-06 18:56:49 +0800

    ETSTs

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -4,6 +4,8 @@ MainFrame::MainFrame() : window(mainWidget) ,kushamiTray(window) {
     window.setWindowTitle("Shiori Kushami Generator");
     mainBox.setAlignment(Qt::AlignCenter);
     // Setup icon list to main window
+	// one more
+	// tyimes
     setupIconList();
     window.setWindowIcon(iconList);
     kushamiTray.setIcon(iconList);
